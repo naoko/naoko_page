ba72050072920b451d3093cbac90eda9ec13cd34
Naoko <naoko@lawlogix.com>
2014-03-15 17:37:51 -0700

    added font awsome

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -2,14 +2,17 @@
 
 angular.module('naokoPageApp')
   .controller('AboutCtrl', function ($scope) {
+
     $scope.profiles = [
         {'site':'blog on blogger', 'url':'http://go-naoko.blogspot.com/'},
-        {'site':'stackoverflow', 'url':'http://stackoverflow.com/users/965808/naoko'},
-        {'site':'github', 'url':'https://github.com/naoko'},
-        {'site':'twitter', 'url':'https://twitter.com/naoko_reeves'},
+        {'site': 'linkedin', url:'https://www.linkedin.com/pub/naoko-reeves/14/939/9a6', 'class':'fa fa-linkedin-square'},
+        {'site':'stackoverflow', 'url':'http://stackoverflow.com/users/965808/naoko', 'class':'fa fa-stack-overflow'},
+        {'site':'github', 'url':'https://github.com/naoko', 'class':'fa fa-github'},
+        {'site':'twitter', 'url':'https://twitter.com/naoko_reeves', 'class':'fa fa-twitter-square'}
     ];
 
     $scope.selectedProfile = function(row) {
         $scope.selectedRow = row;
     };
+
   });
